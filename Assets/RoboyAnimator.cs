1cc3f7c1507052babdaab3ee68a7bc894b53cd8a
missxa <aliona.kharchenko@gmail.com>
2017-07-13 22:56:41 +0200

    with text field for ROS master IP

--- Assets/RoboyAnimator.cs
+++ Assets/RoboyAnimator.cs
@@ -14,6 +14,17 @@ public class RoboyAnimator : MonoBehaviour
     Subscriber sub2;
     Publisher<Float64Msg> pub;
     TcpClient tcp;
+    private bool show = false;
+    private string rosIP = "ROS IP";
+
+    private void OnGUI()
+    {
+        if (show == true)
+        {
+            GUI.skin.textField.fontSize = 30;
+            rosIP = GUI.TextField(new Rect(Screen.width - 250, Screen.height - 40, 250, 40), rosIP, 30);
+        }
+    }
 
     // Use this for initialization
     void Start()
@@ -44,25 +55,34 @@ public class RoboyAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        try
+        if (Input.GetKey(KeyCode.Escape))
         {
-            if (!IsAlive(tcp))
-            {
-                if (tcp != null) tcp.Close();
-                tcp = new TcpClient("10.177.254.226", 9091);
-                node = new Node(new StreamReader(tcp.GetStream()), new StreamWriter(tcp.GetStream()));
-                sub1 = node.Subscribe<SpeechMsg>("/speech_synthesis/speech",
-                                                msg => anim.SetBool("talking", !msg.phoneme.Equals("sil")));
-                sub2 = node.Subscribe<EmotionMsg>("/roboy_face/emotion",
-                                                msg => anim.SetTrigger(msg.emotion));
-                pub = node.Advertise<Float64Msg>("/bla");
-            }
-            pub.Publish(new Float64Msg { data = 1.0 });
-            node.SpinOnce();
+            show = !show;
         }
-        catch (System.Exception e)
+        char x = rosIP[rosIP.Length - 1];
+        if (rosIP[rosIP.Length - 1] == '-')
         {
-            UnityEngine.Debug.LogWarning(e.Message + e.StackTrace);
+
+            try
+            {
+                if (!IsAlive(tcp))
+                {
+                    if (tcp != null) tcp.Close();
+                    tcp = new TcpClient(rosIP.Remove(rosIP.Length - 1), 9091);
+                    node = new Node(new StreamReader(tcp.GetStream()), new StreamWriter(tcp.GetStream()));
+                    sub1 = node.Subscribe<SpeechMsg>("/roboy/cognition/speech/synthesis",
+                                                    msg => anim.SetBool("talking", !msg.phoneme.Equals("sil")));
+                    sub2 = node.Subscribe<EmotionMsg>("/roboy/control/face/emotion",
+                                                    msg => anim.SetTrigger(msg.emotion));
+                    pub = node.Advertise<Float64Msg>("/bla");
+                }
+                pub.Publish(new Float64Msg { data = 1.0 });
+                node.SpinOnce();
+            }
+            catch (System.Exception e)
+            {
+                UnityEngine.Debug.LogWarning(e.Message + e.StackTrace);
+            }
         }
         if(Input.GetKeyDown(KeyCode.L))
             anim.SetTrigger("lookleft");
