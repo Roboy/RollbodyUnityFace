62d33737ea1a6a67cef55303fcb1e75977834aae
missxa <aliona.kharchenko@gmail.com>
2017-12-02 22:07:22 +0100

    additional animations, reading ROS IP from environment

--- Assets/RoboyAnimator.cs
+++ Assets/RoboyAnimator.cs
@@ -14,17 +14,7 @@ public class RoboyAnimator : MonoBehaviour
     Subscriber sub2;
     Publisher<Float64Msg> pub;
     TcpClient tcp;
-    private bool show = false;
-    private string rosIP = "ROS IP";
-
-    private void OnGUI()
-    {
-        if (show == true)
-        {
-            GUI.skin.textField.fontSize = 30;
-            rosIP = GUI.TextField(new Rect(Screen.width - 250, Screen.height - 40, 250, 40), rosIP, 30);
-        }
-    }
+    string ROS_MASTER_IP;
 
     // Use this for initialization
     void Start()
@@ -55,42 +45,48 @@ public class RoboyAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+   
+        try
         {
-            show = !show;
-        }
-        char x = rosIP[rosIP.Length - 1];
-        if (rosIP[rosIP.Length - 1] == '-')
-        {
-
-            try
+            if (!IsAlive(tcp))
             {
-                if (!IsAlive(tcp))
+                UnityEngine.Debug.LogWarning("Connection to the TCP ROS bridge with IP " + ROS_MASTER_IP + " failed, retrying...");
+                if (tcp != null) tcp.Close();
+                                
+                if (System.Environment.GetEnvironmentVariable("ROS_MASTER_URI") == null)
                 {
-                    if (tcp != null) tcp.Close();
-                    tcp = new TcpClient(rosIP.Remove(rosIP.Length - 1), 9091);
-                    node = new Node(new StreamReader(tcp.GetStream()), new StreamWriter(tcp.GetStream()));
-                    sub1 = node.Subscribe<SpeechMsg>("/roboy/cognition/speech/synthesis",
-                                                    msg => anim.SetBool("talking", !msg.phoneme.Equals("sil")));
-                    sub2 = node.Subscribe<EmotionMsg>("/roboy/control/face/emotion",
-                                                    msg => anim.SetTrigger(msg.emotion));
-                    pub = node.Advertise<Float64Msg>("/bla");
+                    UnityEngine.Debug.LogWarning("Environmental variable ROS_MASTER_URI is not set. Assuming ROS is running on localhost.");
+                    ROS_MASTER_IP = "127.0.0.1";
                 }
-                pub.Publish(new Float64Msg { data = 1.0 });
-                node.SpinOnce();
+                else
+                {
+                    System.Uri ROS_MASTER_URI = new System.Uri(System.Environment.GetEnvironmentVariable("ROS_MASTER_URI"));
+                    ROS_MASTER_IP = ROS_MASTER_URI.DnsSafeHost;
+                }
+                                
+                tcp = new TcpClient(ROS_MASTER_IP, 9091);
+                node = new Node(new StreamReader(tcp.GetStream()), new StreamWriter(tcp.GetStream()));
+                sub1 = node.Subscribe<SpeechMsg>("/roboy/cognition/speech/synthesis",
+                                                msg => anim.SetBool("talking", !msg.phoneme.Equals("sil")));
+                sub2 = node.Subscribe<EmotionMsg>("/roboy/control/face/emotion",
+                                                msg => anim.SetTrigger(msg.emotion));
+                pub = node.Advertise<Float64Msg>("/bla");
             }
-            catch (System.Exception e)
-            {
-                UnityEngine.Debug.LogWarning(e.Message + e.StackTrace);
-            }
+            pub.Publish(new Float64Msg { data = 1.0 });
+            node.SpinOnce();
         }
+        catch (System.Exception e)
+        {
+            UnityEngine.Debug.LogWarning(e.Message + e.StackTrace);
+        }
+        
         if(Input.GetKeyDown(KeyCode.L))
             anim.SetTrigger("lookleft");
         if (Input.GetKeyDown(KeyCode.R))
             anim.SetTrigger("lookright");
 
-        //if (Random.value < 0.001f)
-        //    anim.SetTrigger("smileblink");
+        if (Random.value < 0.0001f)
+            anim.SetTrigger("blink");
         //if (Random.value < 0.001f)
         //{
         //    anim.SetTrigger("shy");
