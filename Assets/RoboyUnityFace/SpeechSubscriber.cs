b4d4cb1c00706613f6d13b13869980f010a34ba0
philippwulff <philipp.sylt@gmail.com>
2022-08-16 14:32:16 +0200

    commented out ros1 code; del ros1cs bridge + added ros2cs

--- Assets/RoboyUnityFace/SpeechSubscriber.cs
+++ Assets/RoboyUnityFace/SpeechSubscriber.cs
@@ -2,42 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace RosSharp.RosBridgeClient
-{
-    public class SpeechSubscriber : UnitySubscriber<MessageTypes.RoboyCognition.SpeechSynthesis>
-    {
-        private RoboyAnimator animator;
-        private bool isMessageReceived;
-        private bool talking;
+// namespace RosSharp.RosBridgeClient
+// {
+//     public class SpeechSubscriber : UnitySubscriber<MessageTypes.RoboyCognition.SpeechSynthesis>
+//     {
+//         private RoboyAnimator animator;
+//         private bool isMessageReceived;
+//         private bool talking;
 
-        // Start is called before the first frame update
-        protected override void Start()
-        {
-            base.Start();
-            animator = GetComponent<RoboyAnimator>();
-        }
+//         // Start is called before the first frame update
+//         protected override void Start()
+//         {
+//             base.Start();
+//             animator = GetComponent<RoboyAnimator>();
+//         }
 
-        // Update is called once per frame
-        private void Update()
-        {
-            if (isMessageReceived)
-                ProcessMessage();
-        }
+//         // Update is called once per frame
+//         private void Update()
+//         {
+//             if (isMessageReceived)
+//                 ProcessMessage();
+//         }
 
-        protected override void ReceiveMessage(MessageTypes.RoboyCognition.SpeechSynthesis message)
-        {
-            talking = "sil" != message.phoneme;
-            isMessageReceived = true;
-        }
+//         protected override void ReceiveMessage(MessageTypes.RoboyCognition.SpeechSynthesis message)
+//         {
+//             talking = "sil" != message.phoneme;
+//             isMessageReceived = true;
+//         }
 
-        private void ProcessMessage()
-        {
-            if (isMessageReceived)
-            {
-                Debug.Log("talkigng = " + talking);
-                animator.SetTalking(talking);
-                isMessageReceived = false;
-            }
-        }
-    }
-}
+//         private void ProcessMessage()
+//         {
+//             if (isMessageReceived)
+//             {
+//                 Debug.Log("talkigng = " + talking);
+//                 animator.SetTalking(talking);
+//                 isMessageReceived = false;
+//             }
+//         }
+//     }
+// }
